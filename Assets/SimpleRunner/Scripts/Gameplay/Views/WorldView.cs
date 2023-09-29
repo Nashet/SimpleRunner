596d20e7e80581f715da91102b1ad52e05ed67ac
Nashet <Nashet@users.noreply.github.com>
2023-09-29 12:07:32 +0200

    Add small fixes

--- Assets/SimpleRunner/Scripts/Gameplay/Views/WorldView.cs
+++ Assets/SimpleRunner/Scripts/Gameplay/Views/WorldView.cs
@@ -23,29 +23,29 @@ namespace Nashet.SimpleRunner.Gameplay.Views
 		public void UpdateHappenedHandler(float fixedDeltaTime, Vector2 playerPosition)
 		{
 			var chanceToGenerateBackgroundObject = Random.Range(0f, 1f);
-			Debug.Log(chanceToGenerateBackgroundObject);
-			if (chanceToGenerateBackgroundObject < gameplayConfig.backgroundObjectGenerationChance)
+			if (chanceToGenerateBackgroundObject >= gameplayConfig.backgroundObjectGenerationChance)
 			{
+				return;
+			}
 
-				var randomXScale = Random.Range(4, 13);
-				var randomYScale = Random.Range(5, 23);
+			var randomXScale = Random.Range(4, 13);
+			var randomYScale = Random.Range(5, 23);
 
 
-				var backgroundObject = gameObjectFactory.CreateObject();
+			var backgroundObject = gameObjectFactory.CreateObject();
 
-				backgroundObject.transform.position = new Vector3(playerPosition.x + 20 + randomXScale, randomYScale - 10, 0);
+			backgroundObject.transform.position = new Vector3(playerPosition.x + 20 + randomXScale, randomYScale - 10, 0);
 
-				backgroundObject.transform.localScale = new Vector3(randomYScale, randomXScale, 1);
-				var randomColor = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
-				backgroundObject.GetComponent<SpriteRenderer>().color = randomColor;
-				backgroundObject.SetActive(true);
-				gameObjects.Enqueue(backgroundObject);
+			backgroundObject.transform.localScale = new Vector3(randomYScale, randomXScale, 1);
+			var randomColor = new Color(Random.Range(0, 1f), Random.Range(0, 1f), Random.Range(0, 1f));
+			backgroundObject.GetComponent<SpriteRenderer>().color = randomColor;
+			backgroundObject.SetActive(true);
+			gameObjects.Enqueue(backgroundObject);
 
-				if (gameObjects.Count > gameplayConfig.maxAmountOfBackgroundObjects)
-				{
-					var obj = gameObjects.Dequeue();
-					gameObjectFactory.DestroyObject(obj);
-				}
+			if (gameObjects.Count > gameplayConfig.maxAmountOfBackgroundObjects)
+			{
+				var obj = gameObjects.Dequeue();
+				gameObjectFactory.DestroyObject(obj);
 			}
 		}
 	}
